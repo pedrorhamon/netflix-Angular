1d79fcef23dc08769dd1f993fdb58263e990fa9a
Pedro <pedrorhamon16@gmail.com>
2021-05-05 09:28:07 -0300

    Inserindo o componente list-titles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FullBannerComponent } from './shared/full-banner/full-banner.component';
+import { ListTitlesComponent } from './shared/list-titles/list-titles.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    FullBannerComponent
+    FullBannerComponent,
+    ListTitlesComponent
   ],
   imports: [
     BrowserModule,
