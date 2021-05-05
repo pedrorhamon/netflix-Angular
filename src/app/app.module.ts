1205432471f7110c2aef1b38af03bb1e13470948
Pedro <pedrorhamon16@gmail.com>
2021-05-05 09:32:20 -0300

    Acrescentando o componente footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FullBannerComponent } from './shared/full-banner/full-banner.component';
 import { ListTitlesComponent } from './shared/list-titles/list-titles.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     FullBannerComponent,
-    ListTitlesComponent
+    ListTitlesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
